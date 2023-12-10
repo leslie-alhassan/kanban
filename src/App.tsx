b70a10c0e0578d7c98c0b8ed9ab91a5ec9b8a17a
Leslie Al-Hassan <leslie.k.alhassan@gmail.com>
2023-12-09 21:28:27 -0500

    Add clerk auth

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,88 @@ import './App.scss';
 import LandingPage from './pages/LandingPage/LandingPage';
 import HomePage from './pages/HomePage/HomePage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+  RedirectToSignUp,
+} from '@clerk/clerk-react';
+
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!clerkPubKey) {
+  throw new Error('Missing Publishable Key');
+}
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path='/'
-          element={<LandingPage />}
-        />
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <>
+                <SignedIn>
+                  <HomePage />
+                </SignedIn>
+                <SignedOut>
+                  <LandingPage />
+                </SignedOut>
+              </>
+            }
+          />
 
-        <Route
-          path='/home'
-          element={<HomePage />}
-        />
+          <Route
+            path='/home'
+            element={
+              <>
+                <SignedIn>
+                  <HomePage />
+                </SignedIn>
+                <SignedOut>
+                  <RedirectToSignIn />
+                </SignedOut>
+              </>
+            }
+          />
 
-        <Route
-          path='*'
-          element={<NotFoundPage />}
-        />
-      </Routes>
-    </BrowserRouter>
+          <Route
+            path='/login'
+            element={
+              <>
+                <SignedIn>
+                  <HomePage />
+                </SignedIn>
+                <SignedOut>
+                  <RedirectToSignIn />
+                </SignedOut>
+              </>
+            }
+          />
+
+          <Route
+            path='/sign-up'
+            element={
+              <>
+                <SignedIn>
+                  <HomePage />
+                </SignedIn>
+                <SignedOut>
+                  <RedirectToSignUp />
+                </SignedOut>
+              </>
+            }
+          />
+
+          <Route
+            path='*'
+            element={<NotFoundPage />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </ClerkProvider>
   );
 }
 
